643c8f3b592a7ef1e1652860fddebfa20bca6211
ColdFrenzy <nemesi.95@hotmail.it>
2025-02-17 19:26:07 +0100

    fixed_plots

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -1,10 +1,10 @@
 import os
 # Get IS_RENDER from the environment or default to False
-IS_RENDER = os.getenv("IS_RENDER", "False").lower() == "true"
-# Set the backend conditionally
-if not IS_RENDER:
-    import matplotlib
-    matplotlib.use('Agg')  # Non-interactive backend for headless environments
+# IS_RENDER = os.getenv("IS_RENDER", "False").lower() == "true"
+# # Set the backend conditionally
+# if not IS_RENDER:
+#     import matplotlib
+#     matplotlib.use('Agg')  # Non-interactive backend for headless environments
 import matplotlib.pyplot as plt
 import os
 import numpy as np
@@ -69,59 +69,63 @@ def save_steps_plot(steps_list, save_path="plots", filename="steps_plot.png"):
 
 
 
-def tensordboard_plot(tensorboard_path: str):
-    # Define the path to your TensorBoard logs
-    
-    run_dirs = [os.path.join(tensorboard_path, run) for run in os.listdir(tensorboard_path)]
-
-    # Initialize a list to store the reward values for each run
-    all_rewards = []
-    all_steps = []
-    # Read the event files from each run directory
-    for run in run_dirs:
-        event_acc = event_accumulator.EventAccumulator(run)
-        event_acc.Reload()
-
-        # Assuming the tag is 'reward' in your TensorBoard logs
-
-        # Extract step and reward values
-        steps = [event.step for event in event_acc.Scalars('rollout/ep_rew_mean')]
-        reward_values = [event.value for event in event_acc.Scalars('rollout/ep_rew_mean')]
-
-        # Store rewards of each run
-        all_steps.append(np.array(steps))
-        all_rewards.append(np.array(reward_values))
-
-    # Find the unique set of steps across all runs
-    all_unique_steps = np.unique(np.concatenate(all_steps))
-
-
-    aligned_rewards = []
-    for steps, rewards in zip(all_steps, all_rewards):
-        # Create an interpolation function based on the current run's steps
-        interp_func = interp1d(steps, rewards, kind='linear', bounds_error=False, fill_value="extrapolate")
-        
-        # Interpolate the rewards to match the unique steps
-        new_rewards = interp_func(all_unique_steps)
-        
-        aligned_rewards.append(new_rewards)
-    # Convert the list of rewards to a NumPy array
-    aligned_rewards = np.array(aligned_rewards)
-
-    # Compute the mean and std across all runs (along axis=0 corresponds to different seeds/runs)
-    mean_rewards = np.mean(aligned_rewards, axis=0)
-    std_rewards = np.std(aligned_rewards, axis=0)
-
-    # Plot the results
+def tensordboard_plot(tensorboard_path: str, save_path: str):
+    # Initialize figure for plotting
     plt.figure(figsize=(10, 6))
-    plt.plot(all_unique_steps, mean_rewards, label='Mean Reward', color='blue')
-    plt.fill_between(all_unique_steps, mean_rewards - std_rewards, mean_rewards + std_rewards, color='blue', alpha=0.2, label='Std Deviation')
+    for algorithm_name, log_dir in tensorboard_path.items():
+        all_rewards = []
+        all_steps = []
+        run_dirs = [os.path.join(log_dir, run) for run in os.listdir(log_dir)]
+
+        # Read the event files from each run directory
+        for run in run_dirs:
+            event_acc = event_accumulator.EventAccumulator(run)
+            event_acc.Reload()
+
+
+            # Extract step and reward values
+            steps = [event.step for event in event_acc.Scalars('rollout/ep_rew_mean')]
+            reward_values = [event.value for event in event_acc.Scalars('rollout/ep_rew_mean')]
+
+            # Store rewards of each run
+            all_steps.append(np.array(steps))
+            all_rewards.append(np.array(reward_values))
+
+        # Find the unique set of steps across all runs
+        all_unique_steps = np.unique(np.concatenate(all_steps))
+
+
+        aligned_rewards = []
+        for steps, rewards in zip(all_steps, all_rewards):
+            # Create an interpolation function based on the current run's steps
+            interp_func = interp1d(steps, rewards, kind='linear', bounds_error=False, fill_value="extrapolate")
+            
+            # Interpolate the rewards to match the unique steps
+            new_rewards = interp_func(all_unique_steps)
+            
+            aligned_rewards.append(new_rewards)
+        # Convert the list of rewards to a NumPy array
+        aligned_rewards = np.array(aligned_rewards)
+
+        # Compute the mean and std across all runs (along axis=0 corresponds to different seeds/runs)
+        mean_rewards = np.mean(aligned_rewards, axis=0)
+        std_rewards = np.std(aligned_rewards, axis=0)
+
+    
+        plt.plot(all_unique_steps, mean_rewards, label=f"{algorithm_name}", lw=2)
+        plt.fill_between(all_unique_steps, mean_rewards - std_rewards, mean_rewards + std_rewards, alpha=0.2, )
+    
     plt.xlabel('Steps')
     plt.ylabel('Reward')
-    plt.title('Mean and Std of Rewards Across Different Runs')
     plt.legend()
     plt.show()
 
+
+    # Save the plot to a file (e.g., PNG)
+    plt.savefig(save_path)
+
+    # Close the plot to free up memory
+    plt.close()
     # steps = [event.step for event in event_acc.Scalars('reward')]  # Assuming step is consistent across runs
 
     # # Create a SummaryWriter to log the aggregated data
@@ -137,5 +141,9 @@ def tensordboard_plot(tensorboard_path: str):
 
 
 if __name__ == "__main__":
-    tb_path = ROOT_DIR / "sac_uav_tensorboard" / "5x5" 
-    tensordboard_plot(tensorboard_path=tb_path)
+    name = "5x5"
+    tb_paths = {"SAC": f"sac_uav_tensorboard/{name}",
+               "SAC_HER": f"her_sac_uav_tensorboard/{name}",
+               "SAC_HR": f"sac_hr_uav_tensorboard/{name}"}
+    img_plot = ROOT_DIR / "plots" / f"{name}"
+    tensordboard_plot(tensorboard_path=tb_paths, save_path=img_plot)
